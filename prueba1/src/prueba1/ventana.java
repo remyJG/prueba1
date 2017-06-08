6baa20a36221a4b339b47dfad7053b830c2b992c
PC1\PC <rakeru_emyj6@hotmail.com>
2017-06-08 02:07:49 -0600

    prueba final

--- prueba1/src/prueba1/ventana.java
+++ prueba1/src/prueba1/ventana.java
@@ -1,5 +1,7 @@
 package prueba1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 
 public class ventana extends javax.swing.JFrame {
@@ -45,9 +47,19 @@ public class ventana extends javax.swing.JFrame {
 
         jButton3.setText("INVERTIR CADENA");
         jButton3.setName("invertirCadena"); // NOI18N
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("CALCULAR REGLA DE 3");
         jButton4.setName("regla3"); // NOI18N
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jTextField1.setName(""); // NOI18N
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
@@ -65,6 +77,11 @@ public class ventana extends javax.swing.JFrame {
         jLabel4.setText("Resultado:");
 
         jButton5.setText("LIMPIAR");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -160,25 +177,108 @@ public class ventana extends javax.swing.JFrame {
     public void vocales(){
         String text1 = jTextField1.getText();
         char[] vector = text1.toCharArray();
+        int contador = 0;
         
         for (int x=0;x<vector.length;x++){
             System.out.println("[" + x + "] " + vector[x]);
+            String letra = String.valueOf(vector[x]);
+            if (letra.equalsIgnoreCase("a")){
+                contador = contador + 1;
+            }else  if (letra.equalsIgnoreCase("e")){
+                contador = contador + 1;
+            }else  if (letra.equalsIgnoreCase("i")){
+                contador = contador + 1;
+            }else  if (letra.equalsIgnoreCase("o")){
+                contador = contador + 1;
+            }else  if (letra.equalsIgnoreCase("u")){
+                contador = contador + 1;
+            }
+            System.out.println(contador);
+            if(contador >= 5){
+                jTextField4.setText("CUMPLE");
+            } else{
+                jTextField4.setText("NO CUMPLE");
+            }
         }
+    
+    }
+    
+    public void invertirCadena(){  
+        System.out.println("holis");
+        String text1 = jTextField1.getText();
+        System.out.println(text1);
+        
+        char[] vector1 = text1.toCharArray();
+        char[] reverse1 = new char[vector1.length];
+        int f=0;
+        for (int i= vector1.length-1; i>=0; i--){
+            System.out.println(vector1[i]);
+            
+                reverse1[f] = vector1[i];
+                System.out.println("holis"+ reverse1[f]);
+                f++;
+        }
+        
+        System.out.println("holiscaracolis");
+        String text2 = jTextField2.getText();
+        System.out.println(text2);
+        
+        char[] vector2 = text2.toCharArray();
+        char[] reverse2 = new char[vector2.length];
+        int g=0;
+        for (int j= vector2.length-1; j>=0; j--){
+            System.out.println(vector2[j]);
+            
+                reverse2[g] = vector2[j];
+                System.out.println("holis"+ reverse2[g]);
+                g++;
+        }
+        
+        String salida1 = new String(reverse1);
+        String salida2 = new String(reverse2);
+        System.out.println(salida1);
+        System.out.println(salida2);
+        jTextField4.setText(salida1 + " " + salida2);   
+    }
+    
+    private void regla3(){
+        int text1 = Integer.parseInt(jTextField1.getText());
+        int text2 = Integer.parseInt(jTextField2.getText());
+        int text3 = Integer.parseInt(jTextField3.getText());
+        
+        int resultado1 = text3*text2;
+        int resultado2 = resultado1/text1;
+        
+        System.out.println(resultado2);
+        jTextField4.setText(String.valueOf(resultado2));
     }
     
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
         calcularHipotemusa();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
         vocales();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        invertirCadena();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        regla3();
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        jTextField1.setText(null);
+        jTextField2.setText(null);
+        jTextField3.setText(null);
+        jTextField4.setText(null);
+    }//GEN-LAST:event_jButton5ActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
